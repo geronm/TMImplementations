1a68cfd539d3a7cca3dd5ec4182c45898d7f6297
Geronimo Mirano <geronm@mit.edu>
2018-07-03 20:31:19 -0400

    Added tape-printing function

--- turing.py
+++ turing.py
@@ -70,6 +70,36 @@ class TuringMachine():
 
         return input_list
 
+    def print_tape_state(self, cur_tm_state, cur_tm_head_pos, tape_state, tape_lo, tape_hi):
+        head_msg = '^ %s ' % (cur_tm_state)
+        max_inp_symbol_length = max(max(len(sym) for sym in self.alphabet), len(head_msg)+1)
+
+        alphabet_to_padded = {sym:(sym + ' '*(max_inp_symbol_length - len(sym))) for sym in self.alphabet}
+
+        # First, print the tape symbols.
+        PREFIX='... '
+        SUFFIX='... '
+        tape_print = '%s%s%s' % (PREFIX, ' '.join([alphabet_to_padded[self.default_input]] + [alphabet_to_padded[sym] for sym in tape_state] + [alphabet_to_padded[self.default_input]]), SUFFIX)
+        print (tape_print)
+
+        # Next, print 0-position and head position + state
+        desc_char_array = [' '] * len(tape_print)
+        for i in range(tape_lo-1, tape_hi+1):
+            index_i = len(PREFIX) + ((-tape_lo + i + 1) * (1+max_inp_symbol_length))
+            if index_i > 0 and index_i < len(tape_print):
+                desc_char_array[index_i] = str(i)[0]
+                if len(str(i)) > 1:
+                    desc_char_array[index_i+1] = str(i)[1]
+
+        index_head = len(PREFIX) + ((-tape_lo + cur_tm_head_pos + 1) * (1+max_inp_symbol_length)) + 2
+        for i in range(len(head_msg)):
+            index_write = index_head + i
+            if index_write > 0 and index_write < len(tape_print):
+                desc_char_array[index_write] = head_msg[i]
+        desc_char_str = ''.join(desc_char_array)
+        print (desc_char_str)
+
+
     def simulate(self, input_symbol_list, print_intermediate_tape=True, max_num_iters=500):
         '''
             Simulate the tm. Pretty straightforward implementation.
@@ -95,7 +125,8 @@ class TuringMachine():
         while its < max_num_iters and not halted:
 
             if print_intermediate_tape:
-                print tape_state[:-tape_lo], tape_state[-tape_lo:]
+                # print tape_state[:-tape_lo], tape_state[-tape_lo:]
+                self.print_tape_state(cur_tm_state, cur_tm_head_pos, tape_state, tape_lo, tape_hi)
 
             tape_state_ind = cur_tm_head_pos - tape_lo
 
@@ -135,6 +166,7 @@ class TuringMachine():
             its += 1
 
         if print_intermediate_tape:
-            print tape_state[:-tape_lo], tape_state[-tape_lo:]
+            # print tape_state[:-tape_lo], tape_state[-tape_lo:]
+            self.print_tape_state(cur_tm_state, cur_tm_head_pos, tape_state, tape_lo, tape_hi)
 
-        return accepted, (cur_tm_state, cur_tm_head_pos, tape_lo, tape_hi, tape_state)
+        return accepted, (cur_tm_state, cur_tm_head_pos, tape_state, tape_lo, tape_hi)
