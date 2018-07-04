08410b3596999c4cb4940d63a5ef663ef335522a
Geronimo Mirano <geronm@mit.edu>
2018-07-03 21:11:09 -0400

    Some abstraction. Not that helpful, but eh.

--- turing.py
+++ turing.py
@@ -4,11 +4,51 @@
 # Class to run a Turing machine on an input string. Python 2.
 #
 
+import attr
 import re
 import numpy as np
 
+@attr.s
+class TuringTape(object):
+    cur_tm_state = attr.ib()
+    cur_tm_head_pos = attr.ib()
+    tape_state = attr.ib()
+    tape_lo = attr.ib()
+    tape_hi = attr.ib()
+    default_symbol = attr.ib()
 
-class TuringMachine():
+    def print_tape(self):
+        alphabet = list
+        head_msg = '^%s' % (self.cur_tm_state)
+        max_inp_symbol_length = max(max(len(sym) for sym in self.tape_state), len(head_msg)+1)
+
+        alphabet_to_padded = {sym:(sym + ' '*(max_inp_symbol_length - len(sym))) for sym in self.tape_state}
+
+        # First, print the tape symbols.
+        PREFIX='... '
+        SUFFIX=' ...'
+        tape_print = '%s%s%s' % (PREFIX, ' '.join([alphabet_to_padded[self.default_symbol]] + [alphabet_to_padded[sym] for sym in self.tape_state] + [alphabet_to_padded[self.default_symbol]]), SUFFIX)
+        print (tape_print)
+
+        # Next, print 0-position and head position + state
+        desc_char_array = [' '] * len(tape_print)
+        for i in range(self.tape_lo-1, self.tape_hi+1):
+            index_i = len(PREFIX) + ((-self.tape_lo + i + 1) * (1+max_inp_symbol_length))
+            if index_i > 0 and index_i < len(tape_print):
+                desc_char_array[index_i] = str(i)[0]
+                if len(str(i)) > 1:
+                    desc_char_array[index_i+1] = str(i)[1]
+
+        index_head = len(PREFIX) + ((-self.tape_lo + self.cur_tm_head_pos + 1) * (1+max_inp_symbol_length)) + 1
+        for i in range(len(head_msg)):
+            index_write = index_head + i
+            if index_write > 0 and index_write < len(tape_print):
+                desc_char_array[index_write] = head_msg[i]
+        desc_char_str = ''.join(desc_char_array)
+        print (desc_char_str)
+
+
+class TuringMachine(object):
     def __init__(self, alphabet, rules):
         '''
             Parameters:
@@ -70,36 +110,6 @@ class TuringMachine():
 
         return input_list
 
-    def print_tape_state(self, cur_tm_state, cur_tm_head_pos, tape_state, tape_lo, tape_hi):
-        head_msg = '^ %s ' % (cur_tm_state)
-        max_inp_symbol_length = max(max(len(sym) for sym in self.alphabet), len(head_msg)+1)
-
-        alphabet_to_padded = {sym:(sym + ' '*(max_inp_symbol_length - len(sym))) for sym in self.alphabet}
-
-        # First, print the tape symbols.
-        PREFIX='... '
-        SUFFIX='... '
-        tape_print = '%s%s%s' % (PREFIX, ' '.join([alphabet_to_padded[self.default_input]] + [alphabet_to_padded[sym] for sym in tape_state] + [alphabet_to_padded[self.default_input]]), SUFFIX)
-        print (tape_print)
-
-        # Next, print 0-position and head position + state
-        desc_char_array = [' '] * len(tape_print)
-        for i in range(tape_lo-1, tape_hi+1):
-            index_i = len(PREFIX) + ((-tape_lo + i + 1) * (1+max_inp_symbol_length))
-            if index_i > 0 and index_i < len(tape_print):
-                desc_char_array[index_i] = str(i)[0]
-                if len(str(i)) > 1:
-                    desc_char_array[index_i+1] = str(i)[1]
-
-        index_head = len(PREFIX) + ((-tape_lo + cur_tm_head_pos + 1) * (1+max_inp_symbol_length)) + 2
-        for i in range(len(head_msg)):
-            index_write = index_head + i
-            if index_write > 0 and index_write < len(tape_print):
-                desc_char_array[index_write] = head_msg[i]
-        desc_char_str = ''.join(desc_char_array)
-        print (desc_char_str)
-
-
     def simulate(self, input_symbol_list, print_intermediate_tape=True, max_num_iters=500):
         '''
             Simulate the tm. Pretty straightforward implementation.
@@ -112,11 +122,12 @@ class TuringMachine():
             ---
             accepted, state - accepted is whether the TM accepted, state is info about halt state.
         '''
-        tape_lo = 0
-        tape_hi = len(input_symbol_list)
-        tape_state = list(input_symbol_list)
-        cur_tm_head_pos = 0
-        cur_tm_state = self.start_state
+        tape = TuringTape(tape_lo=0,
+                          tape_hi=len(input_symbol_list),
+                          tape_state=list(input_symbol_list),
+                          cur_tm_head_pos=0,
+                          cur_tm_state=self.start_state,
+                          default_symbol=self.default_input)
 
         its = 0
         halted = False
@@ -125,25 +136,24 @@ class TuringMachine():
         while its < max_num_iters and not halted:
 
             if print_intermediate_tape:
-                # print tape_state[:-tape_lo], tape_state[-tape_lo:]
-                self.print_tape_state(cur_tm_state, cur_tm_head_pos, tape_state, tape_lo, tape_hi)
+                tape.print_tape()
 
-            tape_state_ind = cur_tm_head_pos - tape_lo
+            tape_state_ind = tape.cur_tm_head_pos - tape.tape_lo
 
-            cur_tape_input = tape_state[tape_state_ind]
+            cur_tape_input = tape.tape_state[tape_state_ind]
 
-            print (cur_tm_state, cur_tape_input)
+            print (tape.cur_tm_state, cur_tape_input)
 
-            state_to, input_to, direction = self.rule_dict[(cur_tm_state, cur_tape_input)]
+            state_to, input_to, direction = self.rule_dict[(tape.cur_tm_state, cur_tape_input)]
 
-            cur_tm_state = state_to
-            tape_state[tape_state_ind] = input_to
+            tape.cur_tm_state = state_to
+            tape.tape_state[tape_state_ind] = input_to
 
             # Move head
             if direction == 'left':
-                cur_tm_head_pos -= 1
+                tape.cur_tm_head_pos -= 1
             elif direction == 'right':
-                cur_tm_head_pos += 1
+                tape.cur_tm_head_pos += 1
             elif direction == 'halt_accept':
                 halted = True
                 accepted = True
@@ -156,17 +166,16 @@ class TuringMachine():
                 raise Exception('Bad direction: %s' % direction)
 
             # Lengthen tape if necessary:
-            if cur_tm_head_pos < tape_lo:
-                tape_lo -= 1
-                tape_state.insert(0, self.default_input)
-            elif cur_tm_head_pos >= tape_hi:
-                tape_hi += 1
-                tape_state.insert(0, self.default_input)
+            if tape.cur_tm_head_pos < tape.tape_lo:
+                tape.tape_lo -= 1
+                tape.tape_state.insert(0, self.default_input)
+            elif tape.cur_tm_head_pos >= tape.tape_hi:
+                tape.tape_hi += 1
+                tape.tape_state.insert(0, self.default_input)
 
             its += 1
 
         if print_intermediate_tape:
-            # print tape_state[:-tape_lo], tape_state[-tape_lo:]
-            self.print_tape_state(cur_tm_state, cur_tm_head_pos, tape_state, tape_lo, tape_hi)
+            tape.print_tape()
 
-        return accepted, (cur_tm_state, cur_tm_head_pos, tape_state, tape_lo, tape_hi)
+        return accepted, tape
